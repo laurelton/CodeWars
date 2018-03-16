eb624204c11a056208241da3e97e7cee93865d1e
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-16 18:37:17 -0400

    Revert "First few katas"
    This reverts commit 96aef4e92c7e507c88e16258258f970163b995bb.

---
+++ 8kyu/binToDecimal.js
@@ -0,0 +1,9 @@
+'use strict';
+/**
+ * Complete the function which converts a binary number (given as a string)
+ * to a decimal number.
+ */
+
+function binToDec(str) {
+    return Number.parseInt(Number.parseInt(bin, 2), 10);
+}
