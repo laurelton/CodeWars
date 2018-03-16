96aef4e92c7e507c88e16258258f970163b995bb
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-16 18:35:15 -0400

    First few katas
    This reverts commit 42bcc58cc2f422f2464aed3de219bd340e4dc574.

--- 8kyu/binToDecimal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * Complete the function which converts a binary number (given as a string)
- * to a decimal number.
- */
-
-function binToDec(str) {
-    return Number.parseInt(Number.parseInt(bin, 2), 10);
-}
--- 8kyu/hexToDecimal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/**
- * Complete the function which converts hex number (given as a string)
- * to a decimal number.
- */
-
-function hexToDec(hexString) {
-    return Number.parseInt(Number.parseInt(hexString, 16), 10);
-}
